3b350496324db38d91808a44f6b1f8ad7f6cf618
Lovleen Kaur <sunybuffalo@LOVLEENs-MBP.lan>
2021-09-06 19:28:11 -0400

    LC16

---
+++ LC_16.py
@@ -0,0 +1,33 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        diff = 1001
+        lst = sorted(nums)
+        res = None
+        for i, a in enumerate(lst):
+            l = i + 1
+            r = len(lst) - 1
+
+            while (l < r):
+
+                s = a + lst[l] + lst[r]
+
+                if abs(target - s) < diff:
+
+                    diff = abs(target - s)
+                    res = s
+                else:
+                    diff = diff
+
+                if s == target:
+                    return target
+                if s > target:
+                    r = r - 1
+                else:
+                    l = l + 1
+
+        return res
+
+
+
+
+
